c1f186a8c4b3558e84854b00c60151f07be5ce26
MilindaDissanayake <150835052+MilindaDissanayake@users.noreply.github.com>
2024-12-03 00:19:50 +0530

    add bstrp and css for security attendance

--- warden/security_attendance.php
+++ warden/security_attendance.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$cssPath = "../styles/styles.css";
 include '../config/db.php';
 
 if (!isset($_SESSION['role']) || $_SESSION['role'] != 'warden') {
@@ -40,12 +41,50 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
+<!-- front end -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Security Attendance</title>
+    <link rel="stylesheet" href="<?php echo $cssPath; ?>">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <style>
+        .container {
+            max-width: auto;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        table th,
+        table td {
+            padding: 10px;
+            text-align: center;
+            border: 1px solid #ddd;
+        }
+
+        .message {
+            color: green;
+        }
+
+        .card-header {
+            color: #660097;
+            font-weight: bold;
+
+        }
+    </style>
+
 </head>
+
 <body>
     <?php include 'header.php'; ?>
     <h1>Security Attendance</h1>
@@ -74,4 +113,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <br><a href="dashboard.php">Back to Dashboard</a>
     <?php include 'footer.php'; ?>
 </body>
+
 </html>
